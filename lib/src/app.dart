190f22ba41ed3effe6861e37785781da7323eda4
Simon Horlick <simonhorlick@gmail.com>
2021-08-09 17:14:00 +0800

    Finish drawer

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:plantos/src/pages/crops/appdrawer_bloc.dart';
 import 'package:plantos/src/services/auth_service.dart';
 import 'package:plantos/src/services/crops_service.dart';
 import 'package:plantos/src/services/user_service.dart';
@@ -26,10 +27,12 @@ class App extends StatelessWidget {
             homeWidget = BlocProvider<LoginBloc>(
                 create: (_) => LoginBloc(authService), child: LoginPage());
           } else if (state is AuthAuthenticatedState) {
-            homeWidget = BlocProvider<CropsBloc>(
-                create: (_) =>
-                    CropsBloc(authService, cropsService, userService),
-                child: CropsPage());
+            homeWidget = BlocProvider<AppDrawerBloc>(
+                create: (_) => AppDrawerBloc(authService, userService),
+                child: BlocProvider<CropsBloc>(
+                    create: (_) =>
+                        CropsBloc(authService, cropsService, userService),
+                    child: CropsPage()));
           } else if (state is AuthUninitializedState) {
             homeWidget = Scaffold();
           } else {
--- lib/src/pages/crops/crops_page.dart
+++ lib/src/pages/crops/crops_page.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:plantos/src/pages/crop_details/crop_details_bloc.dart';
@@ -6,18 +5,9 @@ import 'package:plantos/src/pages/crop_details/crop_details_page.dart';
 import 'package:plantos/src/pages/edit_crop/edit_crop_page.dart';
 import 'package:plantos/src/themes/colors.dart';
 import '../../models/crop.dart';
+import 'appdrawer.dart';
 import 'crops_bloc.dart';
-
-class Hamburger extends StatelessWidget {
-  const Hamburger({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return IconButton(
-        onPressed: () => Scaffold.of(context).openDrawer(),
-        icon: Image.asset("assets/hamburger.png"));
-  }
-}
+import 'hamburger.dart';
 
 class CropsPage extends StatefulWidget {
   @override
@@ -135,24 +125,6 @@ class _CropsPageState extends State<CropsPage>
   }
 
   Widget cropsTabViewPage(CropsStateDone state, BuildContext context) {
-    const nameStyle = TextStyle(
-        color: Color(0xFF28183D),
-        fontSize: 18,
-        fontFamily: "Work Sans",
-        fontWeight: FontWeight.w500);
-
-    const emailStyle = TextStyle(
-        color: Color(0x8028183d),
-        fontSize: 14,
-        fontFamily: "Work Sans",
-        fontWeight: FontWeight.normal);
-
-    const menuTextStyle = TextStyle(
-        color: Color(0xff938B9E),
-        fontSize: 18,
-        fontFamily: "Work Sans",
-        fontWeight: FontWeight.w500);
-
     const titleTextStyle = TextStyle(
         color: Color(0xff1D1F21),
         fontSize: 26,
@@ -168,65 +140,7 @@ class _CropsPageState extends State<CropsPage>
         title: Image.asset("assets/logo/withtext.png",
             width: 115.0, height: 27.14),
       ),
-      drawer: Drawer(
-        elevation: 0,
-        // Add a ListView to the drawer. This ensures the user can scroll
-        // through the options in the drawer if there isn't enough vertical
-        // space to fit everything.
-        child: ListView(
-          // Important: Remove any padding from the ListView.
-          // padding: EdgeInsets.zero,
-          children: [
-            Container(
-              child: Padding(
-                padding: const EdgeInsets.only(left: 21.0, right: 21),
-                child: Row(children: [
-                  Center(
-                      child: Image.asset("assets/user.png",
-                          width: 52, height: 52)),
-                  SizedBox(width: 22),
-                  Column(
-                    mainAxisSize: MainAxisSize.min,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text("Adam Sydanus", style: nameStyle),
-                      Text("adam@plant-os.com", style: emailStyle)
-                    ],
-                  )
-                ]),
-              ),
-            ),
-            SizedBox(height: 19),
-            Padding(
-                padding: const EdgeInsets.only(left: 21.0, right: 21),
-                child: Divider(
-                  color: Color(0xffEBEBEB),
-                )),
-            // SizedBox(height: 19),
-            Padding(
-                padding:
-                    const EdgeInsets.only(left: 43.0, right: 21, top: 20.0),
-                child: GestureDetector(
-                    onTap: () {
-                      // TODO: Do this properly.
-                      FirebaseAuth.instance.signOut();
-
-                      // Then close the drawer
-                      Navigator.pop(context);
-                      // Navigator.pushReplacement(
-                      //   context,
-                      // );
-                    },
-                    child: Row(
-                      children: [
-                        Image.asset("assets/icon/logout.png"),
-                        SizedBox(width: 18),
-                        Text('Logout', style: menuTextStyle)
-                      ],
-                    ))),
-          ],
-        ),
-      ),
+      drawer: AppDrawer(),
       body: SafeArea(
         child: Container(
           child: Column(
